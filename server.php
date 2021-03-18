3376c2868373022c24fd66faeb34dee4ece05655
Tanya Leenders <tanya.leenders@gmail.com>
2021-03-18 11:12:40 +0100

    test to repair apostrophe bug

--- server.php
+++ server.php
@@ -46,9 +46,9 @@ if (isset($_POST['register_user'])) {
     } else {
         $name = testInput($_POST["name"]);
         // name contains only letters & whitespaces ?
-        // if (!preg_match("/^[a-zA-Z-\' ]*$/",$name)) {
-        //     $nameError = "Only letters and white space allowed!";
-        // }
+        if (!preg_match("/^[a-zA-Z \'-]*$/",$name)) {
+            $nameError = "Only letters and white space allowed!";
+        }
         if (strlen($name) > 50) {
             $nameError = "Name is too long!";
         }
